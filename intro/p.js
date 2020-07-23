a98615703761242ca61069901122d483fa66df35
alejandroechezria <j_echezuria@gmail.com>
2020-07-23 15:28:45 -0300

    first commit

---
+++ intro/p.js
@@ -0,0 +1,20 @@
+
+ 
+
+var nombre= prompt ( 'qual es tu nombre ');
+
+ //var idade= prompt ('idade');
+var sobrenome= prompt ('sobrenome');
+
+if (nombre=='maria' & sobrenome=="moreno"){
+    alert( 'maria'  + 'moreno ' + 'puedes' + 'continuar');
+}
+ else{
+     alert("persona incorreta")
+ }
+
+ //document.write= '(nombre + idade + sobrenome)';
+
+//alert('nombre' + 'idade');
+//var amigos =['maria','pedro','jose'];
+//console.log(amigos[1])
