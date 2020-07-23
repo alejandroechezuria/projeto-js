c4e3294dc390d31bb63573aed787cfac390ad751
alejandroechezria <j_echezuria@gmail.com>
2020-07-23 17:56:17 -0300

    te

--- intro/outroexercicio.js
+++ intro/outroexercicio.js
@@ -4,10 +4,12 @@ nome=prompt('qual e seu nome ');
 idade=prompt('qual e sua idade');
 
 
-if(idade>=18 && idade <= 45) {
+if(idade>=18 && idade <= 35) {
 
     document.write (nome  +"tiene" + idade + "anos, es maior de idade, ainda e jovem ");
 
+} else if ( idade >35 && idade <= 44 ){
+document.write (nome+'tiene' + idade+ 'esta em a  , crisis dos 40')
 }
 
 else {
